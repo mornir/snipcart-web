41aff669909c73ddc4e9cb6cbc08c9387c76e9c2
mornir <jeromepott@hotmail.com>
2021-02-25 19:10:18 +0100

    Update all deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,9 @@ module.exports = {
   extends: [
     '@nuxtjs',
     'prettier',
-    'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
   ],
-  // required to lint *.vue files
-  plugins: ['prettier'],
   // add your custom rules here
   rules: {
     'no-console': 'off',
